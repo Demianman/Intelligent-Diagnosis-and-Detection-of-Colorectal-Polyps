bce740b15a358dace61525275df9822c5f7df26c
吴宜蔓 <wuyiman@wuyimandembp.mynetworksettings.com>
2025-11-24 13:04:13 -0500

    Remove all assignment files from GitHub tracking

--- hw2_q1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE news.articles (
-    identifier SERIAL PRIMARY KEY,
-    lastBuildDate TIMESTAMP WITH TIME ZONE,
-    Title TEXT,
-    Link TEXT,
-    PubDate TIMESTAMP WITH TIME ZONE,
-    Description TEXT,
-    Source TEXT
-);
--- hw2_q4_1.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE news.articles
-ADD COLUMN IF NOT EXISTS category text DEFAULT 'technology';
-UPDATE news.articles
-SET category = 'technology'
-WHERE category IS NULL;
